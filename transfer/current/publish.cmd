157d5a5f64ab7829cc7dfd36252f79e6e7e2b0dd
William Blum <william.blum@gmail.com>
2007-02-21 14:27:13 +0000

    git-svn-id: svn://localhost@364 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

---
+++ transfer/current/publish.cmd
@@ -0,0 +1,3 @@
+pscp main.pdf blum@mercury.comlab.ox.ac.uk:/auto/fs/websrc/oucl/work/william.blum/transferThesis.pdf
+plink blum@mercury.comlab.ox.ac.uk cd /auto/fs/websrc/oucl/work/william.blum/; /users/blum/packbin/webpub transferThesis.pdf
+pause

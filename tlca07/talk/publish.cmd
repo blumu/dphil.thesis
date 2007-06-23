a1620ad1e0a33b0a5073469757a1b428f9d50ba4
William Blum <william.blum@gmail.com>
2007-06-23 23:47:32 +0000

    git-svn-id: svn://localhost@484 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

---
+++ tlca07/talk/publish.cmd
@@ -0,0 +1,3 @@
+texify.exe --pdf tlca07-talk.tex
+pscp tlca07-talk.pdf blum@mercury.comlab.ox.ac.uk:/auto/fs/websrc/oucl/work/william.blum/
+plink blum@mercury.comlab.ox.ac.uk /users/blum/packbin/webpub /auto/fs/websrc/oucl/work/william.blum/tlca07-talk.pdf

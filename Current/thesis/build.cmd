ae22ad4dafa412c07f93839b8293afe6ed1b12ce
William Blum <william.blum@gmail.com>
2008-06-26 15:27:52 +0000

    git-svn-id: svn://localhost@789 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

--- Current/thesis/build.cmd
+++ Current/thesis/build.cmd
@@ -1,7 +1,7 @@
 latex -aux-directory=TeXAux thesis.tex
-latex -aux-directory=TeXAux thesis.tex
 bibtex TeXAux\thesis
 makeindex -s myown.ist TeXAux\thesis
 makeindex -s myown.ist -o TeXAux\thesis.and TeXAux\thesis.adx
 makeindex -s myown.ist -o TeXAux\thesis.nnd TeXAux\thesis.ndx
+latex -aux-directory=TeXAux thesis.tex
 latex -aux-directory=TeXAux thesis.tex

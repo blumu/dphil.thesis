147ab3de0bf42315c60ca132ad87f957341aa85d
William Blum <william.blum@gmail.com>
2009-01-16 05:56:42 +0000

    Arxiv submission
    git-svn-id: svn://localhost@955 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

---
+++ Current/lmcs-safelambda/zip-arxiv.cmd
@@ -0,0 +1,5 @@
+del lmcs-arxiv.zip
+7z -tzip a lmcs-arxiv.zip lmcs-safelambda-arxiv.tex corresp_overview.texi further.texi safe_gamesem.texi safe_complexity.texi safe_expressivity.texi safelambda.texi lmcs.cls *.sty pstring.pro
+::lmcs-safelambda.tex lmcs-safelambda.pre
+:: 
+

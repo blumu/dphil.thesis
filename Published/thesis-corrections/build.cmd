0dab4e192f6f78fdefdee29924ef7d06b738cf13
William Blum <william.blum@gmail.com>
2009-01-26 16:40:00 +0000

    Version submitted after doing the requested minor corrections.
    git-svn-id: svn://localhost@963 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

---
+++ Published/thesis-corrections/build.cmd
@@ -0,0 +1,34 @@
+::abstract page
+pdflatex -aux-directory=TeXAux abstract_submission.tex
+
+:default
+del /q TeXAux\*.*
+latex -aux-directory=TeXAux thesis.tex
+bibtex TeXAux\thesis
+makeindex -s myown.ist TeXAux\thesis
+::makeindex -s myown.ist -o TeXAux\thesis.and TeXAux\thesis.adx
+::makeindex -s myown.ist -o TeXAux\thesis.nnd TeXAux\thesis.ndx
+latex -aux-directory=TeXAux thesis.tex
+latex -aux-directory=TeXAux thesis.tex
+latex -aux-directory=TeXAux thesis.tex
+dvips -z -t a4 thesis.dvi
+ps2pdf thesis.ps
+goto end
+
+pdftex -ini "&latex" -aux-directory=TeXAux thesis.tex
+
+
+:pdflatex
+del thesis.aux
+texify -p %1.tex
+exit /b
+
+:latex
+del %1.aux
+texify %1.tex
+dvips %1.dvi
+ps2pdf %1.ps
+del /q %1.dvi %1.ps
+exit /b
+
+:end

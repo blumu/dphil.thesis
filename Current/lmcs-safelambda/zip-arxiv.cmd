b58dd4d5af7282c1be5045fbbbc678269b17da31
William Blum <william.blum@gmail.com>
2009-02-17 08:23:51 +0000

    git-svn-id: svn://localhost@967 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

--- Current/lmcs-safelambda/zip-arxiv.cmd
+++ Current/lmcs-safelambda/zip-arxiv.cmd
@@ -1,5 +1,19 @@
+:: prepare the .zip package to be sent to the arXiv website
 del lmcs-arxiv.zip
-7z -tzip a lmcs-arxiv.zip lmcs-safelambda-arxiv.tex corresp_overview.texi further.texi safe_gamesem.texi safe_complexity.texi safe_expressivity.texi safelambda.texi lmcs.cls *.sty pstring.pro
-::lmcs-safelambda.tex lmcs-safelambda.pre
-:: 
 
+:: generate the .bbl file
+if not exist arxiv-aux  md arxiv-aux
+latex -aux-directory=arxiv-aux lmcs-safelambda.tex
+bibtex arxiv-aux\lmcs-safelambda
+latex -aux-directory=arxiv-aux lmcs-safelambda.tex
+latex -aux-directory=arxiv-aux lmcs-safelambda.tex
+copy arxiv-aux\lmcs-safelambda.bbl .
+
+:: build the zip file 
+:: note: the file lmcs-safelambda-arxiv.tex replaces lmcs-safelambda.tex and lmcs-safelambda.pre
+7z -tzip a lmcs-arxiv.zip lmcs-safelambda.tex corresp_overview.texi further.texi safe_gamesem.texi safe_complexity.texi safe_expressivity.texi safelambda.texi lmcs.cls *.sty pstring.pro *.bib *.bbl
+
+:: clean up
+del lmcs-safelambda.bbl
+del /q /s /f arxiv-aux
+rd arxiv-aux

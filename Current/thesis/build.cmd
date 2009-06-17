0f93c39dd4aada68580071efdd9c36981c427348
William Blum <william.blum@gmail.com>
2009-06-17 08:51:23 +0000

    git-svn-id: svn://localhost@983 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

--- Current/thesis/build.cmd
+++ Current/thesis/build.cmd
@@ -1,22 +1,84 @@
-::abstract page
+@if [%1%]==[abstract] goto abstract
+@if [%1%]==[bodleian] goto bodleian
+@if [%1%]==[online] goto online
+@if [%1%]==[reviewer] goto reviewer
+@if [%1%]==[examiner] goto examiner
+@if [%1%]==[all] goto all
+@if [%1%]==[help] (goto help) else goto badargument
+
+exit /b
+
+:badargument
+@echo Unrecognized option!
+@echo.
+
+:help
+@echo Thesis preparation tool.
+@echo.
+@echo Syntax:
+@echo   build [abstract^|bodleian^|online^|reviewer^|examiner^|all]
+@echo.
+@GOTO :EOF
+
+:all
+CALL :abstract
+CALL :bodleian
+CALL :online
+CALL :reviewer
+CALL :examiner
+@GOTO :EOF
+
+
+:abstract
 pdflatex -aux-directory=TeXAux abstract_submission.tex
+@GOTO :EOF
 
-:default
-del /q TeXAux\*.*
-latex -aux-directory=TeXAux thesis.tex
-bibtex TeXAux\thesis
-makeindex -s myown.ist TeXAux\thesis
-::makeindex -s myown.ist -o TeXAux\thesis.and TeXAux\thesis.adx
-::makeindex -s myown.ist -o TeXAux\thesis.nnd TeXAux\thesis.ndx
-latex -aux-directory=TeXAux thesis.tex
-latex -aux-directory=TeXAux thesis.tex
-latex -aux-directory=TeXAux thesis.tex
-dvips -z -t a4 thesis.dvi
-ps2pdf thesis.ps
-goto end
+:bodleian
+CALL :build_pdf Bodleian
+@GOTO :EOF
 
-::pdftex -ini "&latex" -aux-directory=TeXAux thesis.tex
+:online
+CALL :build_pdf OnlineArchive
+@GOTO :EOF
 
+:reviewer
+CALL :build_pdf Reviewer
+@GOTO :EOF
+
+:examiner
+CALL :build_pdf Examiner
+@GOTO :EOF
+
+
+
+:: Subroutine producing the PDF file from the TeX source
+:build_pdf
+set MODE=%1
+rd /q /s TempAux
+md TempAux
+latex -aux-directory=TempAux "\def\%MODE%{}\input{thesis.tex}"
+bibtex TempAux\thesis
+makeindex -s myown.ist TempAux\thesis
+::makeindex -s myown.ist -o TempAux\thesis.and TempAux\thesis.adx
+::makeindex -s myown.ist -o TempAux\thesis.nnd TempAux\thesis.ndx
+latex -aux-directory=TempAux "\def\%MODE%{}\input{thesis.tex}"
+latex -aux-directory=TempAux "\def\%MODE%{}\input{thesis.tex}"
+latex -aux-directory=TempAux "\def\%MODE%{}\input{thesis.tex}"
+dvips -z -t a4 thesis.dvi -o thesis-%MODE%.ps
+ps2pdf thesis-%MODE%.ps thesis-%MODE%.pdf
+rd /q /s TempAux
+@GOTO :EOF
+
+
+
+::::::::::
+::
+:: deprecated compilation methods
+::
+
+:pdftex
+pdftex -ini "&latex" -aux-directory=TempAux "\def\Bodleian{}\input{thesis.tex}"
+exit /b
 
 :pdflatex
 del thesis.aux
@@ -31,4 +93,4 @@ ps2pdf %1.ps
 del /q %1.dvi %1.ps
 exit /b
 
-:end
+:::::::::::::::::::::::

026f9197d9826cb319bc86b9b337ada57bae3531
William Blum <william.blum@gmail.com>
2009-08-18 23:57:51 +0000

    git-svn-id: svn://localhost@1007 79315eeb-8d0d-0410-b3ce-e36577d3b6d3

--- Published/thesis/remack.cmd
+++
@@ -1,5 +0,0 @@
-:: remove acknowledgment from pdf
-@echo off
-pdftk thesis-OnlineArchive.pdf cat 1-3 8-end output thesis-OnlineArchive-NA.pdf
-pdftk thesis-Bodleian.pdf cat 1-2 5-end output thesis-Bodleian-NA.pdf
-pdftk thesis-Print.pdf cat 1-3 8-end output thesis-Print-NA.pdf
